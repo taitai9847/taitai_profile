8a5d5172508288e154b960afcef7f937ad14684b
石川 太洋 <taitai9847@gmail.com>
2020-05-16 22:47:23 +0900

    作成したものを一覧で表示

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,20 @@
 class BoardsController < ApplicationController
     def index
+        @boards = Board.all
     end
 
     def new
+        @board = Board.new
+    end
+
+    def create
+        Board.create(board_params)
+        redirect_to boards_path
+    end
+
+    private
+
+    def board_params
+        params.require(:board).permit(:title, :body)    
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :boards
+  # resources :boards
+  get "boards", to: "boards#index"
+  get "boards/new", to: "boards#new"
+  post "boards", to: "boards#create"
 end
