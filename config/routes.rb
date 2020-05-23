260b97c92453b8af1e9dd3c9e8a5c6665a8df855
石川 太洋 <taitai9847@gmail.com>
2020-05-23 20:57:22 +0900

    work、contactの削除、デザインの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # get 'homes/index'
   get "/homes", to: "homes#index"
-  get "/homes/works", to: "homes#work"
-  get "/homes/contact", to: "homes#contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :boards
 end
